37c674b38674b168203695c2631af5e1b542ca0d
ChinmayVS <chinmayvediyoor@gmail.com>
2025-02-01 22:47:47 +0530

    add to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -53,28 +53,38 @@ products.forEach(product => {
       </div>
 
       <button class="add-to-cart-button button-primary js-add-to-cart"
-      data-product-id="${product.id}>
+      data-product-name="${product.name}">
         Add to Cart
       </button>
     </div>
   `;
 });
 
-console.log(productsHTML);
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-function addToCart(){
-  document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) =>{
-    button.addEventListener('click', () =>{
-      const productId = button.dataset.productId;
+
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) =>{
+  button.addEventListener('click', () =>{
+    const productName = button.dataset.productName;
+
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productName === item.productName){
+        matchingItem= item;
+      }
+    });
+
+    if (matchingItem){
+      matchingItem.quantity+=1;
+    } else {
       cart.push({
-        productId : productId,
+        productName: productName,
         quantity: 1
       })
-      console.log(cart);
-
-    })
+    }
+    console.log(cart);
 
   })
-}
+
+})
